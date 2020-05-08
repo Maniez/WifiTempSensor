b122c4498f22b37065d3b8d6cdc1f8f55338502a
Manuel Diez <manueldiez@Manuels-MBP.fritz.box>
2020-05-08 14:57:04 +0200

    add temp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Homie.h>
+#include <DHT.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
 
 ADC_MODE(ADC_VCC);
 
+#define DHTPIN 5 
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
 HomieNode dummyNode("Dummy", "Dummy", "switch");
 float raw_voltage = 0;
+
+unsigned long previousMillis = 0;
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000; 
 
 void onHomieEvent(const HomieEvent& event) {
   switch(event.type) {
@@ -24,15 +42,51 @@ void onHomieEvent(const HomieEvent& event) {
   }
 }
 
+void loopHandler() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+      Serial.println(t);
+      dummyNode.setProperty("Temp").send(String(t));
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Serial.println(h);
+      dummyNode.setProperty("Humidity").send(String(h));
+    }
+  }
+}
+
 void setup() {
 // put your setup code here, to run once:
     Serial.begin(115200);
     Serial << endl << endl;
 
+    dht.begin();
+
     Homie_setFirmware("TempSensor", "0.0.1");
-    Homie.onEvent(onHomieEvent);
+    //Homie.onEvent(onHomieEvent);
 
     dummyNode.advertise("control").setName("test").setRetained(true).setDatatype("boolean");
+
+    Homie.setLoopFunction(loopHandler);
 
     Homie.setup();
 }
