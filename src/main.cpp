dabe8be81c87140d6aafdc1685288e72c3a41712
Maniez <maniez@gmx.de>
2020-12-28 10:50:11 +0100

    migrate to new PCB

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Ticker.h>
-#include <RemoteDebug.h>  //https://github.com/JoaoLopesF/RemoteDebug
+#include <RemoteDebug.h> //https://github.com/JoaoLopesF/RemoteDebug
 
 #include <IR/IRremoteESP8266.h>
 #include <IR/IRsend.h>
 #include <IR/ir_Fujitsu.h>
 
 // Defines
-#define DHTPIN 5 
+#define DHTPIN 5
 #define DHTTYPE DHT11
 #define MeasurementTimeInSeconds 300
 #define measurementsPerInterval 5
@@ -19,14 +19,14 @@
 #define DEBUG_EN
 
 #ifdef DEBUG_EN
-#define HOST_NAME "tempsensor-3"
+#define HOST_NAME "tempsensor_and_ac_irremote-0"
 #endif
 
 // Gloabal Variables
 DHT dht(DHTPIN, DHTTYPE);
 Ticker measurmentTimer;
 
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+const uint16_t kIrLed = 4; // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRFujitsuAC ac(kIrLed, ARRAH2E);
 
 volatile float t = 0;
@@ -49,28 +49,33 @@ uint32_t wifi_lost_time[10] = {0};
 HomieNode MeasurementNode("Measurements", "Measurements", "string");
 HomieNode KlimaNode("Klima", "Klima", "string");
 
-void doMeasurement(void) {
+void doMeasurement(void)
+{
   // Read five times all values
   t = 0;
   h = 0;
-  for(int i = 0; i < measurementsPerInterval; i++) {
+  for (int i = 0; i < measurementsPerInterval; i++)
+  {
     t += dht.readTemperature();
     h += dht.readHumidity();
     delay(100);
   }
   // Calculate the mean of all values
-  t = t/measurementsPerInterval;
-  h = h/measurementsPerInterval;
+  t = t / measurementsPerInterval;
+  h = h / measurementsPerInterval;
   Serial << "Send Temperatur: " << t << endl;
   Serial << "Send Humidity: " << h << endl;
 }
 
-void enableMeasurment(void){
+void enableMeasurment(void)
+{
   globalEnableMeasurment = true;
 }
 
-void mainHomieLoop(void) {
-  if(globalEnableMeasurment == true) {
+void mainHomieLoop(void)
+{
+  if (globalEnableMeasurment == true)
+  {
     // Do measurement of Temp and humidty
     Serial << "Do Measurement" << endl;
     doMeasurement();
@@ -82,26 +87,73 @@ void mainHomieLoop(void) {
   }
 }
 
-bool switchPowerHandler(const HomieRange& range, const String& value) {
-  if(value.equals("true")){
+void printState()
+{
+  // Display the settings.
+  Serial.println("Fujitsu A/C remote is in the following state:");
+  Serial.printf("  %s\n", ac.toString().c_str());
+  // Display the encoded IR sequence.
+  unsigned char *ir_code = ac.getRaw();
+  Serial.print("IR Code: 0x");
+  for (uint8_t i = 0; i < ac.getStateLength(); i++)
+    Serial.printf("%02X", ir_code[i]);
+  Serial.println();
+}
+
+bool setPowerHandler(const HomieRange &range, const String &value)
+{
+  if (value.equals("true"))
+  {
     Serial.println("Turn On AC");
     ac.setCmd(kFujitsuAcCmdTurnOn);
-    ac.send();
-  } else {
+  }
+  else
+  {
     Serial.println("Turn Off AC");
     ac.setCmd(kFujitsuAcCmdTurnOff);
-    ac.send();
   }
+  ac.send();
   return true;
 }
 
-bool setModeHandler(const HomieRange& range, const String& value) {
-  Serial.println("Set: Mode Auto, 20°C, Fan Auto, Swing On");
-  ac.setSwing(kFujitsuAcSwingHoriz);
-  ac.setMode(kFujitsuAcModeAuto);
-  ac.setFanSpeed(kFujitsuAcFanAuto);
-  ac.setTemp(20);  // 20C
-  ac.setCmd(kFujitsuAcCmdTurnOn);
+bool setTempHandler(const HomieRange &range, const String &value)
+{
+  Serial.println("Set: " + value + "°C");
+  ac.setTemp(value.toInt());
+  ac.send();
+  printState();
+  return true;
+}
+
+bool setSwingHandler(const HomieRange &range, const String &value)
+{
+  if (value.equals("true"))
+  {
+    Serial.println("Set: Swing On");
+    ac.setSwing(kFujitsuAcSwingVert);
+    printState();
+  }
+  else
+  {
+    Serial.println("Set: Swing Off");
+    ac.setSwing(kFujitsuAcSwingOff);
+  }
+  ac.send();
+  return true;
+}
+
+bool setFanHandler(const HomieRange &range, const String &value)
+{
+  Serial.println("Set: Fan " + value);
+  ac.setFanSpeed(value.toInt());
+  ac.send();
+  return true;
+}
+
+bool setModeHandler(const HomieRange &range, const String &value)
+{
+  Serial.println("Set: Fan " + value);
+  ac.setMode(value.toInt());
   ac.send();
   return true;
 }
@@ -110,94 +162,102 @@ bool setModeHandler(const HomieRange& range, const String& value) {
 
 // Process commands from RemoteDebug
 
-void processCmdRemoteDebug() {
-  
-	String lastCmd = Debug.getLastCommand();
+void processCmdRemoteDebug()
+{
 
-	if (lastCmd == "debug1") {
+  String lastCmd = Debug.getLastCommand();
 
-		// Print Debug variables
+  if (lastCmd == "debug1")
+  {
+
+    // Print Debug variables
     debugD("Wifi disconnected %d times", wifi_lost_counter);
-    for(uint32_t i = 0; (i < wifi_lost_counter) && (i < 10); i++) {
-      debugD("Wifi disconnected reason for disconnect %d was: %d at time %d", i+1, wifi_lost_reason[i], wifi_lost_time[i]);
+    for (uint32_t i = 0; (i < wifi_lost_counter) && (i < 10); i++)
+    {
+      debugD("Wifi disconnected reason for disconnect %d was: %d at time %d", i + 1, wifi_lost_reason[i], wifi_lost_time[i]);
     }
 
     debugD("MQTT disconnected %d times", mqtt_lost_counter);
-    for(uint32_t i = 0; (i < mqtt_lost_counter) && (i < 10); i++) {
-      debugD("MQTT disconnected reason for disconnect %d was: %d at time %d", i+1, mqtt_lost_reason[i], mqtt_lost_time[i]);
+    for (uint32_t i = 0; (i < mqtt_lost_counter) && (i < 10); i++)
+    {
+      debugD("MQTT disconnected reason for disconnect %d was: %d at time %d", i + 1, mqtt_lost_reason[i], mqtt_lost_time[i]);
     }
+  }
+  else if (lastCmd == "debug2")
+  {
 
-
-	} else if (lastCmd == "debug2") {
-
-		// Clear Debug variables
+    // Clear Debug variables
     mqtt_lost_counter = 0;
-    for(uint8_t i = 0; i<10; i++){
+    for (uint8_t i = 0; i < 10; i++)
+    {
       mqtt_lost_reason[i] = 0;
     }
 
     wifi_lost_counter = 0;
-    for(uint8_t i = 0; i<10; i++){
+    for (uint8_t i = 0; i < 10; i++)
+    {
       wifi_lost_reason[i] = 0;
     }
   }
 }
 #endif
 
-void onHomieEvent(const HomieEvent& event) {
+void onHomieEvent(const HomieEvent &event)
+{
   String helpCmd;
-  switch(event.type) {
-    case HomieEventType::STANDALONE_MODE:
-      // Do whatever you want when standalone mode is started
-      break;
-    case HomieEventType::CONFIGURATION_MODE:
-      // Do whatever you want when configuration mode is started
-      break;
-    case HomieEventType::NORMAL_MODE:
-      // Do whatever you want when normal mode is started
-      break;
-    case HomieEventType::OTA_STARTED:
-      // Do whatever you want when OTA is started
-      break;
-    case HomieEventType::OTA_PROGRESS:
-      // Do whatever you want when OTA is in progress
+  switch (event.type)
+  {
+  case HomieEventType::STANDALONE_MODE:
+    // Do whatever you want when standalone mode is started
+    break;
+  case HomieEventType::CONFIGURATION_MODE:
+    // Do whatever you want when configuration mode is started
+    break;
+  case HomieEventType::NORMAL_MODE:
+    // Do whatever you want when normal mode is started
+    break;
+  case HomieEventType::OTA_STARTED:
+    // Do whatever you want when OTA is started
+    break;
+  case HomieEventType::OTA_PROGRESS:
+    // Do whatever you want when OTA is in progress
 
-      // You can use event.sizeDone and event.sizeTotal
-      break;
-    case HomieEventType::OTA_FAILED:
-      // Do whatever you want when OTA is failed
-      break;
-    case HomieEventType::OTA_SUCCESSFUL:
-      // Do whatever you want when OTA is successful
-      break;
-    case HomieEventType::ABOUT_TO_RESET:
-      // Do whatever you want when the device is about to reset
-      break;
-    case HomieEventType::WIFI_CONNECTED:
-      // Do whatever you want when Wi-Fi is connected in normal mode
+    // You can use event.sizeDone and event.sizeTotal
+    break;
+  case HomieEventType::OTA_FAILED:
+    // Do whatever you want when OTA is failed
+    break;
+  case HomieEventType::OTA_SUCCESSFUL:
+    // Do whatever you want when OTA is successful
+    break;
+  case HomieEventType::ABOUT_TO_RESET:
+    // Do whatever you want when the device is about to reset
+    break;
+  case HomieEventType::WIFI_CONNECTED:
+    // Do whatever you want when Wi-Fi is connected in normal mode
 #ifdef DEBUG_EN
-      Debug.begin(HOST_NAME);
-      Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
-	    Debug.showColors(true); // Colors
+    Debug.begin(HOST_NAME);
+    Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
+    Debug.showColors(true);   // Colors
 
-      helpCmd = "debug1 - Debug 1\n";
-      helpCmd.concat("debug2 - Debug 2");
+    helpCmd = "debug1 - Debug 1\n";
+    helpCmd.concat("debug2 - Debug 2");
 
-      Debug.setHelpProjectsCmds(helpCmd);
-      Debug.setCallBackProjectCmds(&processCmdRemoteDebug);
+    Debug.setHelpProjectsCmds(helpCmd);
+    Debug.setCallBackProjectCmds(&processCmdRemoteDebug);
 #endif
-      // You can use event.ip, event.gateway, event.mask
-      break;
-    case HomieEventType::WIFI_DISCONNECTED:
-      // Do whatever you want when Wi-Fi is disconnected in normal mode
+    // You can use event.ip, event.gateway, event.mask
+    break;
+  case HomieEventType::WIFI_DISCONNECTED:
+    // Do whatever you want when Wi-Fi is disconnected in normal mode
 #ifdef DEBUG_EN
-      debugD("Wifi disconnected reason %d at time %d", (int)event.wifiReason, (int)millis());
-      wifi_lost_reason[wifi_lost_counter] = event.wifiReason == 0 ? 26 : event.wifiReason;
-      wifi_lost_time[wifi_lost_counter] = millis();
-      wifi_lost_counter++;
+    debugD("Wifi disconnected reason %d at time %d", (int)event.wifiReason, (int)millis());
+    wifi_lost_reason[wifi_lost_counter] = event.wifiReason == 0 ? 26 : event.wifiReason;
+    wifi_lost_time[wifi_lost_counter] = millis();
+    wifi_lost_counter++;
 #endif
-      // You can use event.wifiReason
-      /*
+    // You can use event.wifiReason
+    /*
         Wi-Fi Reason (souce: https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiClientEvents/WiFiClientEvents.ino)
         0  SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
         1  SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
@@ -226,19 +286,19 @@ void onHomieEvent(const HomieEvent& event) {
         24 SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
         25 SYSTEM_EVENT_MAX
       */
-      break;
-    case HomieEventType::MQTT_READY:
-      // Do whatever you want when MQTT is connected in normal mode
-      break;
-    case HomieEventType::MQTT_DISCONNECTED:
-      // Do whatever you want when MQTT is disconnected in normal mode
+    break;
+  case HomieEventType::MQTT_READY:
+    // Do whatever you want when MQTT is connected in normal mode
+    break;
+  case HomieEventType::MQTT_DISCONNECTED:
+    // Do whatever you want when MQTT is disconnected in normal mode
 #ifdef DEBUG_EN
-      mqtt_lost_reason[mqtt_lost_counter] = (uint32_t)event.mqttReason == 0 ? 8 : (uint32_t)event.mqttReason;
-      mqtt_lost_time[mqtt_lost_counter] = millis();
-      mqtt_lost_counter++;
+    mqtt_lost_reason[mqtt_lost_counter] = (uint32_t)event.mqttReason == 0 ? 8 : (uint32_t)event.mqttReason;
+    mqtt_lost_time[mqtt_lost_counter] = millis();
+    mqtt_lost_counter++;
 #endif
-      // You can use event.mqttReason
-      /*
+    // You can use event.mqttReason
+    /*
         MQTT Reason (source: https://github.com/marvinroger/async-mqtt-client/blob/master/src/AsyncMqttClient/DisconnectReasons.hpp)
         0 TCP_DISCONNECTED
         1 MQTT_UNACCEPTABLE_PROTOCOL_VERSION
@@ -249,27 +309,29 @@ void onHomieEvent(const HomieEvent& event) {
         6 ESP8266_NOT_ENOUGH_SPACE
         7 TLS_BAD_FINGERPRINT
       */
-      break;
-    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
-      // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
+    break;
+  case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
+    // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
 
-      // You can use event.packetId
-      break;
-    case HomieEventType::READY_TO_SLEEP:
-      // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
-      break;
-    case HomieEventType::SENDING_STATISTICS:
-      // Do whatever you want when statistics are sent in normal mode
-      break;
+    // You can use event.packetId
+    break;
+  case HomieEventType::READY_TO_SLEEP:
+    // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
+    break;
+  case HomieEventType::SENDING_STATISTICS:
+    // Do whatever you want when statistics are sent in normal mode
+    break;
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  Serial << endl << endl;
-  Homie.disableLedFeedback();
+  Serial << endl
+         << endl;
+  //Homie.disableLedFeedback();
 
-  // Start Homie  
+  // Start Homie
   Homie_setFirmware("TempSensor", "0.2.0");
   Homie.setLoopFunction(mainHomieLoop);
   Homie.onEvent(onHomieEvent); // before Homie.setup()
@@ -280,8 +342,11 @@ void setup() {
   MeasurementNode.advertise("Temperatur").setName("Temp").setRetained(true).setDatatype("float");
   MeasurementNode.advertise("Humidity").setName("Humidity").setRetained(true).setDatatype("float");
 
-  KlimaNode.advertise("power").setName("Powertoggle").setRetained(true).setDatatype("boolean").settable(switchPowerHandler);
-  KlimaNode.advertise("mode").setName("setMode").setRetained(true).setDatatype("boolean").settable(setModeHandler);
+  KlimaNode.advertise("power").setName("Powertoggle").setRetained(true).setDatatype("boolean").settable(setPowerHandler);
+  KlimaNode.advertise("temperatur").setName("Temperatur").setRetained(true).setDatatype("integer").settable(setTempHandler);
+  KlimaNode.advertise("swing").setName("Swing").setRetained(true).setDatatype("boolean").settable(setSwingHandler);
+  KlimaNode.advertise("fan").setName("Fan").setRetained(true).setDatatype("integer").settable(setFanHandler);
+  KlimaNode.advertise("mode").setName("Mode").setRetained(true).setDatatype("integer").settable(setModeHandler);
 
   // Setting default state for A/C.
   // See `fujitsu_ac_remote_model_t` in `ir_Fujitsu.h` for a list of models.
@@ -289,7 +354,7 @@ void setup() {
   ac.setSwing(kFujitsuAcSwingOff);
   ac.setMode(kFujitsuAcModeAuto);
   ac.setFanSpeed(kFujitsuAcFanQuiet);
-  ac.setTemp(20);  // 20C
+  ac.setTemp(20); // 20C
   ac.setCmd(kFujitsuAcCmdTurnOn);
 
   Homie.setup();
@@ -304,16 +369,22 @@ void setup() {
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
   });
-  ArduinoOTA.begin(); 
+  ArduinoOTA.begin();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   ArduinoOTA.handle();
   Homie.loop();
