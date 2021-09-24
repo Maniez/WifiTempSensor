73e5109f04a728c13f78e95fbc5303944e469feb
Manuel Diez <manuel.diez@festo.com>
2021-09-24 14:10:00 +0200

    further

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,15 @@
 #define sleepTimeInSeconds 20
 
 // Settings
-ADC_MODE(ADC_VCC);
 
 // Gloabal Variables
 
 // RTC Memory
-struct NVM{
+struct RTC{
   uint32_t bootCount;
   float batteryLevel;
   bool rainSensor;
-  uint32_t snedCounter;
-} NVM_Memory;
+} RTC_Memory;
 
 
 // Nodes
@@ -31,6 +29,7 @@ void onHomieEvent(const HomieEvent& event) {
       break;
     case HomieEventType::READY_TO_SLEEP:
       Homie.getLogger() << "Ready to sleep" << endl;
+      ESP.rtcUserMemoryWrite(0, (uint32_t *) &RTC_Memory, sizeof(RTC_Memory));
       ESP.deepSleep(sleepTimeInSeconds*1e6);
       break;
     default:
@@ -44,56 +43,55 @@ void setup() {
   yield();
 
   // Init LED and Serial
-  //Homie.disableLedFeedback();
   Serial.begin(115200);
   Serial << endl << endl;
 
   // Print Wake-Up infos
-  Serial << ESP.getResetReason() << endl;
+  Serial << "Boot reason: " << ESP.getResetReason() << endl;
   if (ESP.getResetReason() == "Power On") {
-    NVM_Memory.bootCount = 0;
-    NVM_Memory.batteryLevel = 0;
-    NVM_Memory.rainSensor = false;
-    NVM_Memory.snedCounter = 0;
-    ESP.rtcUserMemoryWrite(0, (uint32_t *) &NVM_Memory, sizeof(NVM_Memory));
+    RTC_Memory.bootCount = 0;
+    RTC_Memory.batteryLevel = 0;
+    RTC_Memory.rainSensor = false;
+    ESP.rtcUserMemoryWrite(0, (uint32_t *) &RTC_Memory, sizeof(RTC_Memory));
   }
 
-  ESP.rtcUserMemoryRead(0, (uint32_t *) &NVM_Memory, sizeof(NVM_Memory));
-  Serial << String(NVM_Memory.bootCount) << endl << endl;
-  NVM_Memory.bootCount++;
-  ESP.rtcUserMemoryWrite(0, (uint32_t *) &NVM_Memory, sizeof(NVM_Memory));
+  ESP.rtcUserMemoryRead(0, (uint32_t *) &RTC_Memory, sizeof(RTC_Memory));
+  Serial << String(RTC_Memory.bootCount) << endl << endl;
+  RTC_Memory.bootCount++;
 
   // Startup finished 
   // Start pre productiv Code
   // Do measurement of Temp,humidty and voltage
 
-  Serial << "Simulate 10s of processing...";
-  delay(10000);
-  Serial << "Decide wheather goto sleep or send data" << endl;
-  if(NVM_Memory.bootCount % 5 != 0) {
-    Serial << "Bootcount modulo" << (NVM_Memory.bootCount % 5) << endl;
+  Serial << "Read Batterylevel and Calc wether send is need" << endl;  
+  float batteryLevel_temp = analogRead(A0) * 420 / 420 / 1000; 
+  Serial << "Batterylevel new: " << batteryLevel_temp << " <-> old: " << RTC_Memory.batteryLevel << endl;
+
+  if(abs(batteryLevel_temp - RTC_Memory.batteryLevel) < 0.01) {
+    Serial << "No need for send" << endl;
+    ESP.rtcUserMemoryWrite(0, (uint32_t *) &RTC_Memory, sizeof(RTC_Memory));
     ESP.deepSleep(sleepTimeInSeconds*1e6);
-  } 
+  } else {
+    RTC_Memory.batteryLevel = batteryLevel_temp;
 
-  ESP.rtcUserMemoryWrite(0, (uint32_t *) &NVM_Memory, sizeof(NVM_Memory));
-  WiFi.forceSleepWake();
-  WiFi.mode(WIFI_STA);
-  // End pre productiv Code
+    WiFi.forceSleepWake();
+    WiFi.mode(WIFI_STA);
+    
+    // Start Homie  
+    //Homie.disableLedFeedback();
+    Homie_setFirmware("TempSensor", "0.1.0");
+    //Homie.setLoopFunction(mainHomieLoop);
+    Homie.onEvent(onHomieEvent);
 
+    /*
+    MeasurementNode.advertise("Temperatur").setName("Temp").setRetained(true).setDatatype("float");
+    MeasurementNode.advertise("Humidity").setName("Humidity").setRetained(true).setDatatype("float");
+    MeasurementNode.advertise("Voltage").setName("Volt").setRetained(true).setDatatype("float");
+    MeasurementNode.advertise("Bootcount").setName("Bootcount").setRetained(true).setDatatype("float");
+    */
 
-  // Start productive code
-  // Start Homie  
-  Homie_setFirmware("TempSensor", "0.1.0");
-  Homie.onEvent(onHomieEvent);
-
-  /*
-  MeasurementNode.advertise("Temperatur").setName("Temp").setRetained(true).setDatatype("float");
-  MeasurementNode.advertise("Humidity").setName("Humidity").setRetained(true).setDatatype("float");
-  MeasurementNode.advertise("Voltage").setName("Volt").setRetained(true).setDatatype("float");
-  MeasurementNode.advertise("Bootcount").setName("Bootcount").setRetained(true).setDatatype("float");
-  */
-
-  Homie.setup();
+    Homie.setup();
+  }
 }
 
 void loop() {
